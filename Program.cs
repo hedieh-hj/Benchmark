a0a4726f05fba4e4e1d95a0bcb7846363fbbc928
hediye jamili <61234450+hedieh-hj@users.noreply.github.com>
2023-09-26 23:22:59 -0800

    benchmark
    install benchmarkdotnet package nuget
    this is console application 
    based .net 6

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System.Linq;
+using System.Text;
+
+//this is console application with .net6
+//instal benchmarkdotnet library 
+
+namespace Benchmarker 
+{
+    class Program { 
+        static void Main(string[] args)
+        {
+            var res = BenchmarkRunner.Run<Demo>();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.Net472, baseline: true)] //use for check even .netframework 
+    [MemoryDiagnoser] //how much memroy do you use
+    public class Demo 
+    {        
+        [Benchmark(Baseline = true)]
+        public string Get()
+        {
+            string output = "";
+
+            for (int i = 0; i < 100; i++)
+            {
+                output += i; 
+            }
+
+            return output;
+        }
+
+        [Benchmark]
+        public string Get2() {
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                output.Append(i);
+            }
+
+            return output.ToString();
+        }
+    }
+}
+
+
